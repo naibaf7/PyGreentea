62ef6934f35be144965a2669a68ba147402af5f7
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-03-29 16:52:02 -0400

    Refactor error scaling & label masking in train()

--- PyGreentea.py
+++ PyGreentea.py
@@ -692,24 +692,15 @@ def train(solver, test_net, data_arrays, train_data_arrays, options):
             net_io.setInputs([data_slice, label_slice, components_slice, data_arrays[0]['nhood']])
             
         if options.loss_function == 'euclid':
-            if(options.scale_error == True):
-                if 'mask' in dataset:
-                    frac_pos = np.clip((label_slice*mask_slice).sum()/mask_slice.sum(),0.05,0.95)
-                    w_pos = 1.0/(2.0*frac_pos)
-                    w_neg = 1.0/(2.0*(1.0-frac_pos))
-                else:
-                    frac_pos = np.clip(label_slice.mean(),0.05,0.95)
-                    w_pos = 1.0/(2.0*frac_pos)
-                    w_neg = 1.0/(2.0*(1.0-frac_pos))
-            else:
-                w_pos = 1
-                w_neg = 1          
-            
-            error_scale_slice = scale_errors(label_slice, w_neg, w_pos)
-            
             if 'mask' in dataset:
                 label_slice = label_slice * mask_slice
-                error_scale_slice = error_scale_slice * mask_slice
+            w_pos = 1.0
+            w_neg = 1.0
+            if options.scale_error:
+                frac_pos = np.clip(label_slice.mean(), 0.05, 0.95)
+                w_pos = w_pos / (2.0 * frac_pos)
+                w_neg = w_neg / (2.0 * (1.0 - frac_pos))
+            error_scale_slice = scale_errors(label_slice, w_neg, w_pos)
             net_io.setInputs([data_slice, label_slice, error_scale_slice])
 
         if options.loss_function == 'softmax':
